3883c5656c09b731ce491ec587b3a4e33cc19b44
Nadia Trigo <nadia.trigo@gmail.com>
2017-04-24 22:47:32 -0300

    limpando o codigo

--- src/components/user_list.js
+++ src/components/user_list.js
@@ -1,68 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {bindActionCreators} from 'redux';
-//import Modal from 'react-modal';
+import { bindActionCreators } from 'redux';
 import AppLink from 'applink';
-
-//import {fetchUsers, getUserById} from '../actions';
-
-// const customStyles = {
-//   content : {
-//     top                   : '50%',
-//     left                  : '50%',
-//     right                 : 'auto',
-//     bottom                : 'auto',
-//     marginRight           : '-50%',
-//     transform             : 'translate(-50%, -50%)'
-//   }
-// };
 
 class UserList extends Component {
 
     constructor(props){
         super(props);
-        this.state = {
-            // openModal: false
-        }
+        this.state = {}
 
-        // this._handleCloseModal = this._handleCloseModal.bind(this);
-        // this._handleClickDetalhes = this._handleClickDetalhes.bind(this);
         this.openAppleStore = this.openAppleStore.bind(this);
     }
-
-    // componentWillMount(){
-    //     //this.props.fetchUsers();
-    // }
-
-    // componentWillUpdate(nextProps){
-    //     //console.log("nextProp: ", nextProps.Pessoas)
-
-    // }
-
-    //_handleClickDetalhes(id){
-        // this.setState({openModal: true});
-        // this.props.getUserById(id);
-    //}
-
-    //_handleCloseModal() {
-        // this.setState({openModal: false});
-    //}
-
-    //renderUser(user){
-        //console.log("user: ", user);
-       //TODO
-       //open a modal with all user information
-       //const click = this._handleClickDetalhes.bind(this, user.id);
-        /*return(            
-            <div id={user.id} className='card card-block' key={user.id}>
-                <h4 className='card-title'>{user.name}</h4>
-                <p className='card-text'>Empresa: {user.company.name}</p>
-                <p className='card-text'>Tel: {user.phone}</p>
-                <a className='btn btn-primary' onClick= { () => {this._handleClickDetalhes(user.id) }}> Detalhes </a>
-                <a className='btn btn-danger'> Excluir </a>
-            </div>
-        );*/
-    //}
 
     openAppleStore(){
         var twitter = new AppLink()
@@ -74,7 +22,6 @@ class UserList extends Component {
             success: function () {},
             nativeURL: encodeURI("twitter://post?message=Look at this awesome thing")
         })
-        //console.log("entrei");
     }
 
     render(){
@@ -85,32 +32,12 @@ class UserList extends Component {
                     Apple Store
                 </button>
 
-                {/*<Modal
-                    isOpen={this.state.openModal}
-                    onRequestClose={this._handleCloseModal}
-                    style={customStyles}
-                    contentLabel="Modal">
-                    
-                    <p>Rua: {this.props.info.users.address}</p>
-                    <p>Numero: {this.props.details.address.suite}</p>
-                    <p>Cidade: {this.props.details.address.city}</p>
-                    <p>CEP: {this.props.details.address.zipcode}</p>
-                   
-                </Modal>*/}
-
-                {/*<div className='user-list'>
-                    {this.props.Pessoas.map(this.renderUser, this)}
-                </div>*/}
             </div>
         );
     }
 }   
 
 function mapStateToProps(){
-    //  console.log("mapStateToProps - info: ", Pessoas['usersInfo'])
-    // return{
-    //      Pessoas
-    // }
     return {}
 }
 
