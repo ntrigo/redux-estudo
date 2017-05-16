7f0f2da543252e19572fa68395e385f55aee59c8
Nadia Trigo <nadia.trigo@gmail.com>
2017-05-15 22:51:57 -0300

    testes com webview, browser ou nativo

--- src/components/user_list.js
+++ src/components/user_list.js
@@ -13,15 +13,36 @@ class UserList extends Component {
     }
 
     openAppleStore(){
-        var twitter = new AppLink()
+        // var twitter = new AppLink()
  
-        twitter.open({
-            error: function () {
-                window.open(encodeURI("https://twitter.com/intent/tweet?text=Look at this awesome thing"), "_blank", "toolbar=0,personalbar=0,resizable,scrollbars,status,width=550,height=450,top=" + Math.round((screen.height - 450)/2) + ",left=" + Math.round((screen.width - 550)/2))
-            },
-            success: function () {},
-            nativeURL: encodeURI("twitter://post?message=Look at this awesome thing")
-        })
+        // twitter.open({
+        //     error: function () {
+        //         window.open(encodeURI("https://twitter.com/intent/tweet?text=Look at this awesome thing"), "_blank", "toolbar=0,personalbar=0,resizable,scrollbars,status,width=550,height=450,top=" + Math.round((screen.height - 450)/2) + ",left=" + Math.round((screen.width - 550)/2))
+        //     },
+        //     success: function () {},
+        //     nativeURL: encodeURI("twitter://post?message=Look at this awesome thing")
+        // })
+
+        var standalone = window.navigator.standalone,
+            userAgent = window.navigator.userAgent.toLowerCase(),
+            safari = /safari/.test( userAgent ),
+            ios = /iphone|ipod|ipad/.test( userAgent );
+
+        if( ios ) {
+            if ( !standalone && safari ) {
+                //browser
+                alert("browser");
+            } else if ( standalone && !safari ) {
+                //standalone
+                alert("standalone");
+            } else if ( !standalone && !safari ) {
+                //uiwebview
+                alert("uiwebview");
+            };
+        } else {
+            //not iOS
+            alert("not iOS");
+        };        
     }
 
     render(){
